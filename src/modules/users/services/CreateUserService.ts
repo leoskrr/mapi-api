6a17a118d265146cf0aa9323e1e3b8754f3ccce9
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 18:52:42 -0400

    feat: create new user

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,45 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email address already in use');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
