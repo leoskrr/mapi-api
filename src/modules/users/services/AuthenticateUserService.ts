5d2d7a7e3ef496be59868895bde6f72aaebac82d
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 19:28:25 -0400

    feat(users): create session of authentication

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,62 @@
+import { sign } from 'jsonwebtoken';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import User from '../infra/typeorm/entities/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: Request): Promise<Response> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user || !user.password) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
