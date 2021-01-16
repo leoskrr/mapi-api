0122d62e9e01e020ebdd45c51f66f4449bebe192
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 19:27:52 -0400

    chore: use router of sessions

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 import { Router } from 'express';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/sessions', sessionsRouter);
 
 export default router;
