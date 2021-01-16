3e7a67685e649e11e273afcd297ea0b8576daca8
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 18:47:49 -0400

    chore: use custom errors when something got wrong

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
@@ -8,6 +11,22 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
