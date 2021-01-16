4dc69b444aebd486441cd318353447e52a7cac25
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 19:48:28 -0400

    chore(users): create fake repository

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from 'uuid';
+
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '../../infra/typeorm/entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+}
+
+export default UsersRepository;
