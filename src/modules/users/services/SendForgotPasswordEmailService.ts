38f701a8163e9262d25563e4f544496b10a2698b
Leonardo Viana <sonleo2013@gmail.com>
2021-01-16 21:05:58 -0400

    chore(email): check if the receiver exists

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 // import AppError from '@shared/errors/AppError';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 // import User from '../infra/typeorm/entities/User';
 
@@ -21,6 +22,12 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: Request): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists.');
+    }
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido',
