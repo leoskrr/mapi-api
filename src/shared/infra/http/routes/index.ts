89d316f2ee2f0fdf04ec96db3eae3ed52b8f6286
Leonardo Viana <sonleo2013@gmail.com>
2021-01-17 16:07:41 -0400

    feat(users): show profile

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 import { Router } from 'express';
 
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
 router.use('/password', passwordRouter);
+router.use('/profile', profileRouter);
 
 export default router;
