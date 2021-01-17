94e9329a821979843d6e85411ab3238ebf255c15
Leonardo Viana <sonleo2013@gmail.com>
2021-01-17 16:41:39 -0400

    chore(users): validate data with celebrate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UsersControllers from '../controllers/UsersControllers';
 
 const usersRouter = Router();
 const usersControllers = new UsersControllers();
 
-usersRouter.post('/', usersControllers.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersControllers.create,
+);
 
 export default usersRouter;
